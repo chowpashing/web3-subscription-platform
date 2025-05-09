fb4ab2a1430ab767e18c64edd3fe943d1f2fe9dc
zhoubaicheng <chowpashing@gmail.com>
2025-05-09 19:46:26 +0800

    usermanagement 01

--- backend/subscription/views.py
+++ backend/subscription/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from .serializers import SubscriptionSerializer
 from django.utils import timezone
 from datetime import timedelta
+from django.utils.dateformat import DateFormat
 
 # Create your views here.
 
@@ -40,7 +41,7 @@ class UserSubscriptionStatusView(APIView):
         user = request.user
         subscriptions = Subscription.objects.filter(user=user).order_by('-payment_time')
         if not subscriptions.exists():
-            return Response({'status': 'no_subscription', 'message': '当前用户无订阅记录。'}, status=status.HTTP_200_OK)
+            return Response({'status': 'no_subscription', 'message': 'No subscription record.'}, status=status.HTTP_200_OK)
 
         now = timezone.now()
         result = []
@@ -48,24 +49,24 @@ class UserSubscriptionStatusView(APIView):
             serializer = SubscriptionSerializer(subscription)
             data = serializer.data
 
-            # 判断订阅状态
             if subscription.expiration_date < now:
                 data['status_description'] = 'expired'
-                data['message'] = '您的订阅已过期。'
+                data['message'] = 'Your subscription has expired.'
                 subscription.active = False
                 subscription.save()
             elif subscription.active:
                 trial_end_time = subscription.payment_time + timedelta(days=subscription.trial_period_days)
                 if subscription.trial_period_days > 0 and now < trial_end_time:
                     data['status_description'] = 'trial'
-                    data['message'] = '您当前处于试用期。'
+                    formatted_time = DateFormat(trial_end_time).format('Y-m-d H:i')
+                    data['message'] = f'Before {formatted_time}, you can'
                     data['cancel_subscription_url'] = '/api/subscription/cancel'
                 else:
                     data['status_description'] = 'subscribed'
-                    data['message'] = '您当前已订阅。'
+                    data['message'] = 'You are currently subscribed.'
             else:
                 data['status_description'] = 'cancelled_not_expired'
-                data['message'] = '您的订阅已取消，将在到期日后失效。'
+                data['message'] = 'Your subscription has been cancelled, but it will remain active until its expiration date.'
             result.append(data)
 
         return Response(result, status=status.HTTP_200_OK)
